95321f0edf9decb1b3c959ab637e010e9d02796e
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-01-09 18:58:06 -0300

    first commit

---
+++ seção 2 javascript básico/aula 020/index.js
@@ -0,0 +1,11 @@
+// - Operadores aritméticos: +, -, *, /
+
+const num1 = 10;
+const num2 = 3;
+
+console.log(num1 + num2);  // - Adição
+console.log(num1 - num2);  // - Subtração
+console.log(num1 * num2);  // - Multiplicação
+console.log(num1 / num2);  // - Divisão
+console.log(num1 ** num2);  // - Potenciação
+console.log(num1 % num2);  // - Resto da divisão
