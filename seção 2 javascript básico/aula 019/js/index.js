7bc3ead855cc5e99dcb5b4e43ab5b04a40b32264
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-01-09 18:10:47 -0300

    first commit

---
+++ seção 2 javascript básico/aula 019/js/index.js
@@ -0,0 +1,6 @@
+const nome = 'Allan';  // - String
+const nome1 = "Allan";  // - String
+const num1 = 10;  // - Number
+const num2 = 10.25  // Number
+let nomeAluno;  // - Underfined --> Não aponta para nanhum local na memória.
+let sobrenomeAluno = null;  // - Nulo --> Não aponta para nanhum local na memória.
