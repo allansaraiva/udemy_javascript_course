15d7caf63a7486af28f5c32e03cf763a762af01f
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-01-10 01:01:53 -0300

    first commit

---
+++ seção 2 javascript básico/aula 021/js/index.js
@@ -0,0 +1,9 @@
+let numero1 = prompt('Digite um número:');
+let numero2 = prompt('Digite outro número:');
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+const resultadoDaSoma = numero1 + numero2;
+
+alert(`A soma dos números é: ${resultadoDaSoma}`);
