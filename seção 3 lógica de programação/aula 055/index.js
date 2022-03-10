9ed2363da7cd0ec49daac809532b63a866a59aea
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-03-10 17:58:08 -0300

    for in

---
+++ seção 3 lógica de programação/aula 055/index.js
@@ -0,0 +1,16 @@
+/* const fruits = ['Banana', 'Lemon', 'Watermelon', 'Apple'];
+
+// For in --> Lê os índices ou as chaves do objeto.
+for (let i in fruits) {
+  console.log(fruits[i]);
+} */
+
+const person = {
+  name: 'Allan',
+  surname: 'Saraiva',
+  age: 21
+};
+
+for (let key in person) {
+  console.log(person[key]);
+}
