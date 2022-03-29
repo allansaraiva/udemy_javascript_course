e723f8a08697ba74d5649434ab7da142fac1a21f
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-03-29 17:40:59 -0300

    functions

--- seção 4 funções (avançado)/aula 069/index.js
+++ seção 4 funções (avançado)/aula 069/index.js
@@ -1,11 +1,4 @@
-function conta(operador, acumulador, ...números) {
-  for(let número of números) {
-    if(operador === '+') acumulador += número;
-    if(operador === '-') acumulador -= número;
-    if(operador === '/') acumulador /= número;
-    if(operador === '*') acumulador *= número;
-  }
-
-  console.log(acumulador);
-}
-conta('/', 0, 10, 20, 30, 40, 50);
+const conta = (...args) => {
+  console.log(args);
+};
+conta('+', 0, 10, 20, 30, 40)
